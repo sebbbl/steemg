a882ea394bec67acabdb658130b534d3f5362738
dapeng <baydap@gmail.com>
2018-04-05 07:24:41 +0200

    Initial commit

---
+++ server.R
@@ -0,0 +1,162 @@
+library(XML)
+library(RCurl)
+library(rlist)
+library(wordcloud)
+library(tm)
+library(httr)
+library(jsonlite)
+library(lubridate)
+library(VennDiagram)
+library(RODBC)
+library(mongolite)
+library(ggplot2)
+library(openair)
+library(lattice)
+# Sys.setenv(TZ='GMT')
+
+options(shiny.maxRequestSize = 9*1024^2, scipen=999)
+
+server <- function(input, output, session) {
+  # observeEvent(input$fr_id, {myid <- input$fr_id})
+  zh <- data.frame(matrix(unlist(strsplit(readLines('./www/zh.csv', encoding = 'UTF-8'), ',')), ncol = 2, byrow = TRUE), stringsAsFactors = FALSE)
+  names(zh) <- c('en', 'zh')
+  ###### steemguides ######
+  steemguides <- read.csv('https://raw.githubusercontent.com/pzhaonet/keller/master/mr/steemguides.txt', stringsAsFactors = FALSE, encoding = 'UTF-8', quote = '')
+  names(steemguides) <- substr(names(steemguides), 3, nchar(names(steemguides))-1)
+  steemguides$date <- as.Date(steemguides$date)
+  steemguides$author <- gsub('"', '', steemguides$author)
+  steemguides$author <- paste0('<a href="https://steemit.com/@', steemguides$author, '">@', steemguides$author , '</a>')
+  steemguides$title <- gsub('"', '', steemguides$title)
+  steemguides$url <- gsub('"', '', steemguides$url)
+  steemguides$github <- gsub('"', '', steemguides$github)
+  steemguides$title <- paste0('<a href="https://steemit.com', steemguides$url, '">', steemguides$title , '</a>')
+  steemguides$cum_posts <- 1:nrow(steemguides)
+  steemguides$cum_payout <- cumsum(steemguides$payout)
+  steemguides$cum_votes <- cumsum(steemguides$votes)
+  steemguidesslmax <- nrow(steemguides)
+  steemguides_author <- read.csv('https://raw.githubusercontent.com/pzhaonet/keller/master/mr/steemguides_author.txt', stringsAsFactors = FALSE, encoding = 'UTF-8', quote = '')
+  names(steemguides_author) <- substr(names(steemguides_author), 3, nchar(names(steemguides_author))-1)
+  steemguides_author$author <- gsub('"', '', steemguides_author$author)
+  steemguides_author$author <- paste0('<a href="https://steemit.com/@', steemguides_author$author, '">@', steemguides_author$author , '</a>')
+  steemguidesaslmax <- nrow(steemguides_author)
+  output$steemguides_dt1 = renderDataTable({
+    mymatsteemguides <- steemguides
+    mymatsteemguides <- mymatsteemguides[, c(1:3, 5:7)]
+    steemguidesN <- 'payout'
+    mymatsteemguides[, 'Rank'] <- floor(rank(-mymatsteemguides[, input$steemguidesN]))
+    mymatsteemguides <- mymatsteemguides[rev(order(mymatsteemguides[, 'date'])), ]
+    # steemguides_colsel <- c('date', 'author', 'title', 'payout','votes', 'score', 'prize')
+    mymatsteemguidesout <- mymatsteemguides[, c('Rank', input$steemguides_colsel)]
+    names(mymatsteemguidesout) <- zh$zh[match(names(mymatsteemguidesout), zh$en)]
+    mymatsteemguidesout
+  },
+  options = list(lengthMenu = c(10, 50, 100, steemguidesslmax), pageLength = 10), escape = FALSE
+  )
+  
+  output$steemguides_dt2 = renderDataTable({
+    mymatsteemguidesa <- steemguides_author
+    # mraN <- 'payout'
+    # mymatmra[, 'Rank'] <- floor(rank(-mymatmra[, mraN]))
+    mymatsteemguidesa[, 'Rank'] <- floor(rank(-mymatsteemguidesa[, input$steemguidesaN]))
+    mymatsteemguidesa <- mymatsteemguidesa[order(mymatsteemguidesa[, 'Rank']), ]
+    # mra_colsel <- c('posts', 'payout','votes', 'character')
+    # mymatmraout <- mymatmra[, c('Rank', mra_colsel)]
+    mymatsteemguidesaout <- mymatsteemguidesa[, c('Rank', input$steemguidesa_colsel)]
+    names(mymatsteemguidesaout) <- zh$zh[match(names(mymatsteemguidesaout), zh$en)]
+    mymatsteemguidesaout
+  },
+  options = list(lengthMenu = c(10, 50, 100, steemguidesaslmax), pageLength = 10), escape = FALSE
+  )
+  steemguidesxlim <- range(steemguides$date)
+  output$steemguides_plot1 <- renderPlot({
+    plotdate(steemguides$date, steemguides$cum_votes, mylegend = 'Cumulative Votes', myxlim = steemguidesxlim, mycol = 'blue')
+  })
+  output$steemguides_plot2 <- renderPlot({
+    plotdate(steemguides$date, steemguides$cum_payout, mylegend = 'Cumulative Payout (SBD)', myxlim = steemguidesxlim, mycol = 'red')
+  })
+  output$steemguides_plot3 <- renderPlot({
+    plotdate(steemguides$date, steemguides$cum_posts, mylegend = 'Cumulative Posts', myxlim = steemguidesxlim)
+  })
+  
+  ###### Monthly review ######
+  mr <- read.csv('https://raw.githubusercontent.com/pzhaonet/keller/master/mr/mr.txt', stringsAsFactors = FALSE, encoding = 'UTF-8', quote = '')
+  names(mr) <- substr(names(mr), 3, nchar(names(mr))-1)
+  mr$date <- gsub('"', '', mr$date)
+  mr$date <- as.Date(mr$date)
+  mr$author <- gsub('"', '', mr$author)
+  mr$author <- paste0('<a href="https://steemit.com/@', mr$author, '">@', mr$author , '</a>')
+  mr$title <- gsub('"', '', mr$title)
+  mr$url <- gsub('"', '', mr$url)
+  mr$reviewed <- gsub('"', '', mr$reviewed)
+  mr$title <- paste0('<a href="https://steemit.com', mr$url, '">', mr$title , '</a>')
+  
+  mr1 <- mr[mr$author != '<a href="https://steemit.com/@rivalhw">@rivalhw</a>',]
+  mr1$prize <- gsub('"', '', mr1$prize)
+  mr1$cum_posts <- 1:nrow(mr1)
+  mr1$cum_payout <- cumsum(mr1$payout)
+  mr1$cum_votes <- cumsum(mr1$votes)
+  mr1slmax <- nrow(mr1)
+  
+  mr3 <- mr[mr$author == '<a href="https://steemit.com/@rivalhw">@rivalhw</a>',]
+  mr3slmax <- nrow(mr3)
+  
+  mr_author <- read.csv('https://raw.githubusercontent.com/pzhaonet/keller/master/mr/mr_author.txt', stringsAsFactors = FALSE, encoding = 'UTF-8', quote = '')
+  names(mr_author) <- substr(names(mr_author), 3, nchar(names(mr_author))-1)
+  mr_author$author <- gsub('"', '', mr_author$author)
+  mr_author$author <- paste0('<a href="https://steemit.com/@', mr_author$author, '">@', mr_author$author , '</a>')
+  mr_author$character <- gsub('"', '', mr_author$character)
+  mr_author <- mr_author[mr_author$author != '<a href="https://steemit.com/@rivalhw">@rivalhw</a>',]
+  mraslmax <- nrow(mr_author)
+  
+  output$mr_dt1 = renderDataTable({
+    mymatmr <- mr1
+    mymatmr <- mymatmr[, c(1:3, 5:8, 10)]
+    mrN <- 'payout'
+    mymatmr[, 'Rank'] <- floor(rank(-mymatmr[, input$mrN]))
+    mymatmr <- mymatmr[rev(order(mymatmr[, 'date'])), ]
+    mr_colsel <- c('date', 'author', 'title', 'payout','votes', 'score', 'prize', 'reviewed')
+    mymatmrout <- mymatmr[, c('Rank', input$mr_colsel)]
+    names(mymatmrout) <- zh$zh[match(names(mymatmrout), zh$en)]
+    mymatmrout
+  },
+  options = list(lengthMenu = c(10, 50, 100, mr1slmax), pageLength = 10), escape = FALSE
+  )
+  
+  output$mr_dt3 = renderDataTable({
+    mymatmr <- mr3
+    mymatmr <- mymatmr[, c(1:3)]
+    mr_colsel <- c('date', 'author', 'title')
+    mymatmrout <- mymatmr
+    names(mymatmrout) <- zh$zh[match(names(mymatmrout), zh$en)]
+    mymatmrout
+  },
+  options = list(lengthMenu = c(10, 50, 100, mr3slmax), pageLength = 10), escape = FALSE
+  )
+  
+  output$mr_dt2 = renderDataTable({
+    mymatmra <- mr_author
+    # mraN <- 'payout'
+    # mymatmra[, 'Rank'] <- floor(rank(-mymatmra[, mraN]))
+    mymatmra[, 'Rank'] <- floor(rank(-mymatmra[, input$mraN]))
+    mymatmra <- mymatmra[order(mymatmra[, 'Rank']), ]
+    # mra_colsel <- c('posts', 'payout','votes', 'character')
+    # mymatmraout <- mymatmra[, c('Rank', mra_colsel)]
+    mymatmraout <- mymatmra[, c('Rank', input$mra_colsel)]
+    names(mymatmraout) <- zh$zh[match(names(mymatmraout), zh$en)]
+    mymatmraout
+  },
+  options = list(lengthMenu = c(10, 50, 100, mraslmax), pageLength = 10), escape = FALSE
+  )
+  mrxlim <- range(mr$date)
+  output$mr_plot1 <- renderPlot({
+    plotdate(mr1$date, mr1$cum_votes, mylegend = 'Cumulative Votes', myxlim = mrxlim, mycol = 'blue')
+  })
+  output$mr_plot2 <- renderPlot({
+    plotdate(mr1$date, mr1$cum_payout, mylegend = 'Cumulative Payout (SBD)', myxlim = mrxlim, mycol = 'red')
+  })
+  output$mr_plot3 <- renderPlot({
+    plotdate(mr1$date, mr1$cum_posts, mylegend = 'Cumulative Posts', myxlim = mrxlim)
+  })
+  
+  
+}
---
+++ ui.R
@@ -0,0 +1,204 @@
+library(shiny)
+
+ui <- fluidPage(
+  tags$head(includeScript("google-analytics.js")),
+  headerPanel("steemg, a collection of steem contests, Steem 各种活动汇总"),
+  tabsetPanel(
+    ###### steemguides ######
+    tabPanel("Steem 指南",
+             sidebarLayout(
+               sidebarPanel(
+                 p(),
+                 img(src="https://steemitimages.com/DQmc9ka9n5aVok9ShgzmuswUVjMKnJXWkSYfhTyXtKLr41c/banner.jpg", width="300"),
+                 h2("《Steem 指南》书稿"),
+                 h4("用途: "),
+                 "展示《Steem 指南》书稿的相关文章和作者。", 
+                 h4("Features: "),
+                 "To display the participants in the book Steem Guides.", 
+                 p(),
+                 hr(),
+                 h4('用法: '),
+                 p(),
+                 "- 单击文章标题或作者会跳转至 steemit。有的浏览器因安全级别高而屏蔽了跳转，改用请用 ctrl + 鼠标左键单击即可;",
+                 p(),
+                 "- 可以选择第一列“座次”按哪个指标排序;",
+                 p(),
+                 "- 可以选择第一列“座次”按哪个指标排序;",
+                 p(),
+                 "- 可以选择显示哪些列;",
+                 p(),
+                 "- 点击表格顶部标题可以正反排序;",
+                 p(),
+                 "- 表格右上角搜索栏可以在全表搜索，底部末行可以关键词过滤。",
+                 p(),
+                 hr(),
+                 h4("联系"),
+                 '本网站归', a(href = 'https://steemit.com/@dapeng', '@dapeng'), ' 所有。欢迎提供建议。', 
+                 p(),
+                 img(src="steemit-watermark.png", width="131", height="38"),
+                 hr(),
+                 wellPanel(
+                   h4("累计文章数量"),
+                   plotOutput('steemguides_plot3', height = 200),
+                   hr(),
+                   h4("累计获赞数量"),
+                   plotOutput('steemguides_plot1', height = 200),
+                   hr(),
+                   h4("累计文章收益"),
+                   plotOutput('steemguides_plot2', height = 200)
+                 )
+                 
+               ),
+               mainPanel(
+                 wellPanel(
+                   h3("文章榜："),
+                   hr(),
+                   checkboxGroupInput(
+                     inputId = "steemguides_colsel",
+                     label   = "选择显示哪些列:",
+                     choices = c('date', 'author', 'title', 'payout','votes', 'github'), #'level', 
+                     selected = c('date', 'author', 'title', 'payout','votes', 'github'),
+                     inline = TRUE
+                   ),
+                   hr(),
+                   radioButtons(
+                     inputId = "steemguidesN",
+                     label   = "座次依据:",
+                     # label   = "Rank in:",
+                     choices = c('payout','votes'),
+                     selected = 'payout',
+                     inline = TRUE
+                   ),
+                   hr(),
+                   
+                   dataTableOutput('steemguides_dt1')
+                 ),
+                 wellPanel(
+                   h3("人物榜："),
+                   hr(),
+                   checkboxGroupInput(
+                     inputId = "steemguidesa_colsel",
+                     label   = "选择显示哪些列:",
+                     choices = c('author', 'posts', 'payout','votes'),
+                     selected = c('author', 'posts', 'payout','votes'),
+                     inline = TRUE
+                   ),
+                   hr(),
+                   radioButtons(
+                     inputId = "steemguidesaN",
+                     label   = "座次依据:",
+                     choices = c('posts', 'payout','votes'),
+                     selected = 'posts',
+                     inline = TRUE
+                   ),
+                   hr(),
+                   dataTableOutput('steemguides_dt2')
+                 )),
+               position = "right"
+             )
+    ),
+    
+    
+    ###### Monthly Review ######
+    tabPanel("月旦评",
+             sidebarLayout(
+               sidebarPanel(
+                 p(),
+                 img(src="https://steemitimages.com/DQmRdNg6SDwvbAXZoyJgyDEZZS29hnenDBPFw68WxWzzMji/monthlyreview.jpg", width="300"),
+                 h2("月旦评"),
+                 h4("用途: "),
+                 "展示月旦评文章和作者。", 
+                 p(),
+                 hr(),
+                 h4('用法: '),
+                 p(),
+                 "- 单击文章标题或作者会跳转至 steemit。有的浏览器因安全级别高而屏蔽了跳转，改用请用 ctrl + 鼠标左键单击即可;",
+                 p(),
+                 "- 可以选择第一列“座次”按哪个指标排序;",
+                 p(),
+                 "- 可以选择第一列“座次”按哪个指标排序;",
+                 p(),
+                 "- 可以选择显示哪些列;",
+                 p(),
+                 "- 点击表格顶部标题可以正反排序;",
+                 p(),
+                 "- 表格右上角搜索栏可以在全表搜索，底部末行可以关键词过滤。",
+                 p(),
+                 hr(),
+                 h4("联系"),
+                 '关于月旦评，请联系活动发起人和主办人', a(href = 'https://steemit.com/@rivalhw', '@rivalhw。'),'本网站归', a(href = 'https://steemit.com/@dapeng', '@dapeng'), ' 所有。欢迎提供建议。', 
+                 p(),
+                 h4("致谢"),
+                 '月旦评 logo 由', a(href = 'https://steemit.com/@lemooljiang', '@lemooljiang'), ' 设计制作。', '琅琊榜的创意来自', a(href = 'https://steemit.com/@angelina6688', '@angelina6688。'), 
+                 p(),
+                 p(),
+                 img(src="steemit-watermark.png", width="131", height="38"),
+                 hr(),
+                 wellPanel(
+                   h4("累计文章数量"),
+                   plotOutput('mr_plot3', height = 200),
+                   hr(),
+                   h4("累计获赞数量"),
+                   plotOutput('mr_plot1', height = 200),
+                   hr(),
+                   h4("累计文章收益"),
+                   plotOutput('mr_plot2', height = 200)
+                 )
+                 
+               ),
+               mainPanel(
+                 wellPanel(
+                   h3("文章榜："),
+                   hr(),
+                   checkboxGroupInput(
+                     inputId = "mr_colsel",
+                     label   = "选择显示哪些列:",
+                     choices = c('date', 'author', 'title', 'payout','votes', 'prize', 'reviewed'), #'score'), #'level', 
+                     selected = c('date', 'author', 'title', 'payout','votes', 'prize', 'reviewed'), #'score'),
+                     inline = TRUE
+                   ),
+                   hr(),
+                   radioButtons(
+                     inputId = "mrN",
+                     label   = "座次依据:",
+                     # label   = "Rank in:",
+                     choices = c('payout','votes'), #'score'),
+                     selected = 'payout',
+                     inline = TRUE
+                   ),
+                   hr(),
+                   
+                   dataTableOutput('mr_dt1')
+                 ),
+                 wellPanel(
+                   h3("档案馆："),
+                   hr(),
+                   dataTableOutput('mr_dt3')
+                 ),
+                 wellPanel(
+                   h3("人物榜："),
+                   hr(),
+                   checkboxGroupInput(
+                     inputId = "mra_colsel",
+                     label   = "选择显示哪些列:",
+                     choices = c('author', 'posts', 'payout','votes'), #'character'),
+                     selected = c('author', 'posts', 'payout','votes'), #'character'),
+                     inline = TRUE
+                   ),
+                   hr(),
+                   radioButtons(
+                     inputId = "mraN",
+                     label   = "座次依据:",
+                     choices = c('posts', 'payout','votes'),
+                     selected = 'posts',
+                     inline = TRUE
+                   ),
+                   hr(),
+                   dataTableOutput('mr_dt2')
+                 )
+               ),
+               position = "right"
+             )
+    )
+  )
+)
